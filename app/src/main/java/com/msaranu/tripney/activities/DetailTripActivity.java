e321c7c8f97fbebcf101b8eaaa56596c3ab07ebb
Malleswari Saranu <msaranu@Malleswaris-MacBook-Pro.local>
2018-02-16 20:28:04 -0800

    Replace viewpager with Botton Navigation Bar

--- app/src/main/java/com/msaranu/tripney/activities/DetailTripActivity.java
+++ app/src/main/java/com/msaranu/tripney/activities/DetailTripActivity.java
@@ -1,13 +1,23 @@
 package com.msaranu.tripney.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.msaranu.tripney.R;
 import com.msaranu.tripney.adapters.TripDetailPagerAdapter;
+import com.msaranu.tripney.fragments.EventDetailFragment;
+import com.msaranu.tripney.fragments.TripDetailFragment;
+import com.msaranu.tripney.fragments.TripThingsToDoFragment;
+import com.msaranu.tripney.fragments.TripWishListFragment;
 import com.msaranu.tripney.models.Trip;
 
 public class DetailTripActivity extends AppCompatActivity {
@@ -19,20 +29,58 @@ public class DetailTripActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_trip);
         Bundle extras = getIntent().getExtras();
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
         trip = extras.getParcelable("trip_obj");
+
+        //Default Load
+        final FragmentTransaction fragmentTripDetail = fragmentManager.beginTransaction();
+        fragmentTripDetail.replace(R.id.flContainer, TripDetailFragment.newInstance(trip)).commit();
+
+        // handle navigation selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_trip_detail:
+                                final FragmentTransaction fragmentTripDetail = fragmentManager.beginTransaction();
+                                fragmentTripDetail.replace(R.id.flContainer, TripDetailFragment.newInstance(trip)).commit();
+                                return true;
+                            case R.id.action_trip_events:
+                                final FragmentTransaction fragmentTripEvents = fragmentManager.beginTransaction();
+                                fragmentTripEvents.replace(R.id.flContainer, TripThingsToDoFragment.newInstance(trip)).commit();
+                                return true;
+                            case R.id.action_wish_list:
+                                final FragmentTransaction fragmentWishList = fragmentManager.beginTransaction();
+                                fragmentWishList.replace(R.id.flContainer, TripWishListFragment.newInstance(trip)).commit();
+                                return true;
+
+                            case R.id.action_expenses:
+                                final FragmentTransaction fragmentExpenses= fragmentManager.beginTransaction();
+                                fragmentExpenses.replace(R.id.flContainer, TripWishListFragment.newInstance(trip)).commit();
+                                return true;
+
+                            default:
+                                return true;
+
+                        }
+                    }
+                }
+        );
+
         // Get the ViewPager and set it's PagerAdapter so that it can display items
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new TripDetailPagerAdapter(getSupportFragmentManager(), trip));
+        //ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        //viewPager.setAdapter(new TripDetailPagerAdapter(getSupportFragmentManager(), trip));
 
         // Give the PagerSlidingTabStrip the ViewPager
-        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        //PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         // Attach the view pager to the tab strip
-        tabsStrip.setViewPager(viewPager);
+        //tabsStrip.setViewPager(viewPager);
 
 
     }
-
-
 
 }
 
